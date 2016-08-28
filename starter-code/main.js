f5a41807771baf7563249133b5a07f64edd52d64
Ramandeep Kaur <ramandeepkaur@Ramans-Air.local>
2016-08-28 16:34:05 -0700

    Add if-else statement to compare cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,22 @@ var cardOne = 'queen';
 var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'king';
+
+if (cardThree == cardFour) {
+	alert("you found a match");
+}
+else {
+	alert("Sorry, try again");
+}
+if (cardOne == cardTwo) {
+	alert ("You found a match");
+}
+else {
+	alert ("Sorry, try again");
+}
+if (cardOne == cardThree) {
+	alert ("You found a match");
+}
+else {
+	alert ("Sorry, try again")
+}
