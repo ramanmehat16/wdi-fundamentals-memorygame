10ac90484b11de86a3e6ab9d19b83fc41265916e
Ramandeep Kaur <ramandeepkaur@Ramans-Air.local>
2016-08-29 11:50:23 -0700

    Add image to king and queen

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,8 +1,12 @@
 console.log("JS file is connected to HTML! Woo!")
+/*
 var cardOne = 'queen';
 var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'king';
+*/
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 /*
 if (cardThree == cardFour) {
 	alert("you found a match");
@@ -23,13 +27,35 @@ else {
 	alert ("Sorry, try again")
 }
 */
-
+function isMatch(cards) {
+	if (cards[0] == cards[1]) {
+		alert("It's a match");
+	}
+	else {
+		alert("Sorry, try again");
+	}
+}
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') == "king") {
+		this.innerHTML = '<img src="KH.png" alt="King of Spades" />';	
+	} 
+	else {
+	this.innerHTML = '<img src="queen-spades.png" alt="Queen of hearts" />';	
+	}
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
+}
 var createBoard = function () {
 	var gameBoard = document.getElementById("game-board");
-	for (var i=0; i < 4; i++) {
+	for (var i=0; i < cards.length; i++) {
 		var cardDiv = document.createElement("div");
 		cardDiv.className = "card";
+		cardDiv.setAttribute('data-card', cards[i]);
+		cardDiv.addEventListener('click', isTwoCards);
 		gameBoard.appendChild (cardDiv);
 	}
 }
-createBoard ()
+createBoard ();
